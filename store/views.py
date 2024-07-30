0cbb1ebf03c9f868b1afb6fa7e6630eeda96d7fa
Bakaev_Olimjon <olimbakaev08@gmial.com>
2024-07-30 22:26:43 +0500

    added registration functionality

--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from . models import Product
+from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 
 
@@ -24,7 +28,7 @@ def login_user(request):
             messages.success(request, ('You Have Been Logged In'))
             return redirect('home') 
         else:
-            messages.success(request, ('There was an error, please try again..'))
+            messages.error(request, ('There was an error, please try again..'))
             return redirect('login')
     else:
 
@@ -36,3 +40,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You have been logged out'))
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have registered successfully!"))
+            return redirect('home')
+        else:
+            messages.error(request, ("Whoops! There was a problem registering. Please, try again"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
