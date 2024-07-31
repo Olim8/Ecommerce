5c731bf6817218540e768d29aa3a0c0e845a096e
Bakaev_Olimjon <olimbakaev08@gmial.com>
2024-07-31 14:19:20 +0500

    added category pages

--- store/views.py
+++ store/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
+
+
+
+def category(request, foo):
+    # Replace hyphens with spaces
+    foo = foo.replace('-', ' ')
+    # Grab the category from the url
+    try:
+        # Look up the category
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success(request, ("That category doesn't exist.."))
+        return redirect('home')
+
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
 
 
 
@@ -62,6 +83,3 @@ def register_user(request):
     
 
 
-def product(request, pk):
-    product = Product.objects.get(id=pk)
-    return render(request, 'product.html', {'product':product})
