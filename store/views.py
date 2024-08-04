ebfe93c7c145c4ef934407195a2bfce3cffa0464
Bakaev_Olimjon <olimbakaev08@gmial.com>
2024-08-05 00:36:10 +0500

    Added User Profile page

--- store/views.py
+++ store/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
+
+
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Your Info has been updated...')
+            return redirect('home')
+        return render(request, 'update_info.html', {'form':form})
+    else:
+        messages.success(request, 'You must be logged in to access that page')
+        return redirect('home')
+
 
 
 
@@ -103,8 +121,8 @@ def register_user(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, ("You have registered successfully!"))
-            return redirect('home')
+            messages.success(request, ("Username Created - Please Fill Out Your User Info Below"))
+            return redirect('update_info')
         else:
             messages.error(request, ("Whoops! There was a problem registering. Please, try again"))
             return redirect('register')
